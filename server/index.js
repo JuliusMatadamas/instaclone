3a98e31808fda4bf7b56187ef7e213bea8a034a5
JuliusMatadamas <JuliusMatadamas@outlook.com>
2023-05-22 19:51:03 -0500

    Se crea la conexion a MongoDB.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("Starting");
+const mongoose = require('mongoose')
+require('dotenv').config({path:'.env'});
+
+mongoose.connect(process.env.BD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log(`Error code: ${err.code}`, `Error message: ${err.message}`);
+})
