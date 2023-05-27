88b8ec63dff68e376e3ee8c424cc2ebc38807ec9
JuliusMatadamas <JuliusMatadamas@outlook.com>
2023-05-26 19:23:20 -0500

    Se agrega apollo-server al proyecto

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require('./gql/schema')
+const resolvers = require('./gql/resolvers')
 require('dotenv').config({path:'.env'});
 
 mongoose.connect(process.env.BD, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Connected to MongoDB');
+    // console.log('Connected to MongoDB');
+    server().then(() => {
+
+    });
 }).catch(err => {
     console.log(`Error code: ${err.code}`, `Error message: ${err.message}`);
 })
+
+async function server() {
+    const serverApollo = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+    serverApollo.listen().then(({ url }) => {
+        console.log(`Server ready at the url ${url}`);
+    })
+}
