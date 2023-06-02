945763bdcbbdced8660f245fce224c5235076b1b
JuliusMatadamas <JuliusMatadamas@outlook.com>
2023-06-02 18:32:18 -0500

    Se crea el mutation para guardar el usuario en MongoDB.

--- server/gql/resolvers.js
+++ server/gql/resolvers.js
@@ -1,9 +1,42 @@
+const User = require("../models/user");
+
 const resolvers = {
     Query: {
         // User
         getUser: () => {
             console.log('Obteniendo usuario');
             return null;
+        }
+    },
+
+    Mutation: {
+        // User
+        register: async (_, { input }) => {
+            const newUser = input;
+            newUser.email = newUser.email.toLowerCase();
+            newUser.password = newUser.password.toLowerCase();
+            const { email, username, password } = newUser;
+
+            // Revisar si existe el email
+            const foundEmail = await User.findOne({ email });
+            if (foundEmail) throw new Error("El email ya se encuentra en uso.");
+
+            // Revisar si el username está en uso
+            const foundUsername = await User.findOne({ username });
+            if (foundUsername) throw new Error("El nombre de usuario ya se encuentra en uso.");
+
+            // Encriptar el password
+            // ...
+
+            try {
+                const user = new User(newUser);
+                user.save();
+                return user;
+            } catch (e) {
+                console.log(e);
+            }
+
+            return input;
         }
     }
 }
